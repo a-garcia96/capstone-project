d356c6f5e2b8ee5e0965049f2502165d2003fc43
a-garcia96 <alex.g9415@gmail.com>
2023-01-04 21:51:50 -0800

    markup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from "react";
+import image from "../assets/restaurantfood.jpg";
 
 const Header = () => {
   return (
-    <header>
-        <h1>Little Lemon</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur debitis tempora facere consectetur quas reiciendis voluptatem! Blanditiis nostrum commodi labore reiciendis cupiditate alias eius, voluptatum officiis laudantium error, quas voluptatibus!</p>
+    <header className="header">
+      <div className="header__col-1">
+        <h1 className="header__title">Little Lemon</h1>
+        <h2 className="header__location">Chicago</h2>
+        <p className="header__text">
+          We are a family owned mediterranean restaurant, focused on traditional
+          recipes served with a modern twist.
+        </p>
+        <button className="header__ctaBtn">Reserve a Table</button>
+      </div>
+      <div className="header__col-2">
+        <img
+          className="header__image"
+          src={image}
+          alt="Bruschetta on a board"
+        />
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
