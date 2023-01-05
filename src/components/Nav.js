a9a8aba8fbf6d1247b705c1c38606d80a0b79ba6
a-garcia96 <alex.g9415@gmail.com>
2023-01-04 21:21:54 -0800

    completed functionality for slide out nav and styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,56 @@
-import React from 'react'
-import Logo from '../assets/Logo.svg'
-import Menu from '../assets/hamburgerMenuIcon.svg'
+import React from "react";
+import { useRef } from "react";
+
+import Logo from "../assets/Logo.svg";
+import Menu from "../assets/hamburgerMenuIcon.svg";
 
 const Nav = () => {
-    return (
-        <nav className='nav'>
-            <div className='nav__header'>
-                <img src={Logo} alt="Little Lemon Logo" className='nav__logo' />
-                <img src={Menu} alt="menu button" />
-            </div>
-            <ul className='nav__list'>
-                <li className='nav__item'><a className='nav__link'>Home</a></li>
-                <li className='nav__item'><a className='nav__link'>About</a></li>
-                <li className='nav__item'><a className='nav__link'>Menu</a></li>
-                <li className='nav__item'><a className='nav__link'>Reservations</a></li>
-                <li className='nav__item'><a className='nav__link'>Order Online</a></li>
-                <li className='nav__item'><a className='nav__link'>Login</a></li>
-            </ul>
-        </nav>
-    )
-}
 
-export default Nav
+    const navRef = useRef(null)
+
+    const handleClick = (e) => {
+        // console.log(navRef.current);
+        const nav = navRef.current;
+        const navItemList = document.querySelectorAll(".nav__item")
+
+        nav.classList.toggle("show--navbar")
+
+        navItemList.forEach(navItem => {
+            navItem.classList.toggle("show--navItem")
+            console.log(navItem.classList)
+        })
+    }
+
+  return (
+    <nav className="nav">
+      <div className="nav__header">
+        <img src={Logo} alt="Little Lemon Logo" className="nav__logo" />
+        <button onClick={handleClick}>
+          <img src={Menu} alt="menu button" />
+        </button>
+      </div>
+      <ul ref={navRef} className="nav__list">
+        <li className="nav__item">
+          <a className="nav__link">Home</a>
+        </li>
+        <li className="nav__item">
+          <a className="nav__link">About</a>
+        </li>
+        <li className="nav__item">
+          <a className="nav__link">Menu</a>
+        </li>
+        <li className="nav__item">
+          <a className="nav__link">Reservations</a>
+        </li>
+        <li className="nav__item">
+          <a className="nav__link">Order Online</a>
+        </li>
+        <li className="nav__item">
+          <a className="nav__link">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
