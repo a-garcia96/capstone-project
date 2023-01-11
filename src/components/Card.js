556e5fa742908324e084c27a52a7d3099fb45e52
a-garcia96 <alex.g9415@gmail.com>
2023-01-10 21:10:51 -0800

    added classnames

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,13 +4,13 @@ const Card = ({ recipeTitle, recipePrice, recipeDesc, recipeImg }) => {
   return (
     <div className='card'>
         <div className="card__image">
-
+          <img src={recipeImg} />
         </div>
-        <div className="card__information">
-            <h3>{recipeTitle}</h3>
-            <p>{recipePrice}</p>
-            <p>{recipeDesc}</p>
-            <button>Order a delivery</button>
+        <div className="clearfix card__information">
+            <h3 className="card__title">{recipeTitle}</h3>
+            <p className='card__price'>{recipePrice}</p>
+            <p className='card__desc'>{recipeDesc}</p>
+            <button className='btn'>Order a delivery</button>
         </div>
     </div>
   )
