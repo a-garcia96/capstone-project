7f1f32f932ac01b5e7cf600ed64f4ae584e83ca3
a-garcia96 <alex.g9415@gmail.com>
2023-01-11 19:43:30 -0800

    markup additions

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from "react";
+import deliveryBike from "../assets/delivery-bike.svg";
 
 const Card = ({ recipeTitle, recipePrice, recipeDesc, recipeImg }) => {
   return (
-    <div className='card'>
-        <div className="card__image">
-          <img src={recipeImg} />
+    <div className="card">
+      <div className="card__image">
+        <img src={recipeImg} />
+      </div>
+      <div className="card__information">
+        <div className="card__row">
+          <h3 className="card__title">{recipeTitle}</h3>
+          <p className="card__price">${recipePrice}</p>
         </div>
-        <div className="clearfix card__information">
-            <h3 className="card__title">{recipeTitle}</h3>
-            <p className='card__price'>{recipePrice}</p>
-            <p className='card__desc'>{recipeDesc}</p>
-            <button className='btn'>Order a delivery</button>
-        </div>
+        <p className="card__desc">{recipeDesc}</p>
+        <button className="card__btn">
+          <a href="#">Order a delivery</a>
+          <img
+            className="card__icon"
+            src={deliveryBike}
+            alt="icon of a delivery bike"
+          />
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
