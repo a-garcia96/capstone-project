5d8a67f0d4290cd57f2535c54cee8408437f83f1
Alex Garcia <alex.g9415@gmail.com>
2023-03-08 16:00:49 -0800

    footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,46 @@
 import React from 'react'
 
+import image from "../assets/restaurantfood.jpg";
+
 const Footer = () => {
   return (
     <footer className='footer'>
-        <h2>This is the footer!</h2>
+      <div className="container">
+        <div className="footer__row">
+          <div className="col">
+            <img src={image} alt="" />
+          </div>
+          <div className="col">
+            <h3>Navigation</h3>
+            <ul className="nav__list--footer">
+            <li className="nav__item">
+              <a className="nav__link">Home</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link">About</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link">Menu</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link">Reservations</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link">Order Online</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link">Login</a>
+            </li>
+          </ul>
+          </div>
+          <div className="col">
+            <h3>Contact</h3>
+          </div>
+          <div className="col">
+            <h3>Social Media</h3>
+          </div>
+        </div>
+      </div>
     </footer>
   )
 }
