a3c8ce3ef8f7c75341f2d365e2d8a6ac9dc811e3
a-garcia96 <alex.g9415@gmail.com>
2023-03-20 19:27:03 -0700

    new page

---
+++ src/routes/Order.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Nav from '../components/Nav'
+import Footer from '../components/Footer'
+
+const Order = () => {
+  return (
+    <>
+        <Nav />
+        <header>
+            <div className="container">
+                <p>Too Tired to Dine-In?</p>
+                <h1>Order Online</h1>
+            </div>
+        </header>
+        <main>
+            <section>
+                <h2></h2>
+            </section>
+        </main>
+        <Footer />
+    </>
+  )
+}
+
+export default Order
